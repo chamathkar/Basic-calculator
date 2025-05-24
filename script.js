de92765a77b297f5ef8906cc29ebb672b0e0b998
chamathkar <chamathkar0027@gmail.com>
2025-05-24 22:41:39 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// let CurrentVal='';
+// document.querySelector('#display').value=CurrentVal;
+let CurrentVal = "";
+
+function append(char) {
+  CurrentVal += char;
+  document.querySelector('#display').value = CurrentVal;
+}
+
+function clearDisplay() {
+  CurrentVal = "";
+  document.querySelector('#display').value = "";
+}
+
+function calculate() {
+  try {
+    const result = eval(CurrentVal);
+    document.querySelector('#display').value = result;
+    CurrentVal = result.toString();
+  } catch {
+    document.querySelector('#display').value = "Error";
+    CurrentVal = "";
+  }
+}
